7f73031830adf0b5ef19d4652e534308ad02c76a
grégory LOMBARD--PARADIS <gregory332000@hotmail.fr>
2023-02-09 15:24:03 +0100

    Finalisation

--- test/calculator/calculator_test.py
+++ test/calculator/calculator_test.py
@@ -11,21 +11,66 @@ class TestCalculator(unittest.TestCase):
         result = self.calc.add(1, 2)
         self.assertEqual(result, 3)
 
+        result = self.calc.add(-1, -2)
+        self.assertEqual(result, -3)
+
+        result = self.calc.add(2.5, 1.5)
+        self.assertEqual(result, 4)
+
+        result = self.calc.add("a", "b")
+        self.assertEqual(result, "ab")
+
     def test_subtraction(self):
         result = self.calc.subtract(2, 1)
         self.assertEqual(result, 1)
+
+        result = self.calc.add(-1, -2)
+        self.assertEqual(result, 1)
+
+        result = self.calc.add(2.5, 1.5)
+        self.assertEqual(result, 1)
+
+        result = self.calc.add("a", "b")
+        self.assertEqual(result, "ab")
 
     def test_multiplication(self):
         result = self.calc.multiply(2, 3)
         self.assertEqual(result, 6)
 
+        result = self.calc.add(-1, -2)
+        self.assertEqual(result, -2)
+
+        result = self.calc.add(2.5, 1.5)
+        self.assertEqual(result, 3.75)
+
+        result = self.calc.add("a", "b")
+        self.assertEqual(result, "ab")
+
     def test_division(self):
         result = self.calc.divide(6, 3)
         self.assertEqual(result, 2)
 
+        result = self.calc.add(-1, -2)
+        self.assertEqual(result, -0.5)
+
+        result = self.calc.add(2.5, 1.5)
+        self.assertEqual(result, 2.08)
+
+        result = self.calc.add("a", "b")
+        self.assertEqual(result, "ab")
+
     def test_power(self):
         result = self.calc.power(2, 3)
         self.assertEqual(result, 8)
+
+        result = self.calc.add(-2, -2)
+        self.assertEqual(result, 4)
+
+        result = self.calc.add(2.5, 1.5)
+        self.assertEqual(result, 3,95)
+
+        result = self.calc.add("a", "b")
+        self.assertEqual(result, "ab")
 
     def test_square_root(self):
         result = self.calc.square_root(4)
